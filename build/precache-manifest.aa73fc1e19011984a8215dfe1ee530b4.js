5ec841f4666913b60a6c555e34e742a5d7c33182
Steven Balerna <stevenbalerna@Stevens-MacBook-Air.local>
2019-09-03 18:28:03 -0400

    added helmets

---
+++ build/precache-manifest.aa73fc1e19011984a8215dfe1ee530b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ebe62240ce72c4d78f664cde5110e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b85bea6e9a6fe8972a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "59b85bea6e9a6fe8972a",
+    "url": "/static/js/main.b02c1d69.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Helmets from "./helmets.json";
+import Header from "./components/Header";
+import { throwStatement } from '@babel/types';
+import GameBoard from "./components/GameBoard"
+import ResultPanel from "./components/ResultPanel"
+
+const shuffleArray = array => {
+
+  for (let i = array.length - 1; i > 0; i--) {
+
+    let randomIdx = Math.floor(Math.random() * (i + 1))
+    let randomValue = array[randomIdx]
+
+    array[randomIdx] = array[i]
+    array[i] = randomValue
+
+  }
+
+  return array;
+
+}
+
+class App extends Component {
+  state = {
+    clicks: [],
+    clickCount: 0,
+    wins: 0,
+    losses: 0,
+    highScore: 0,
+    Helmets,
+    results: "rules"
+  };
+  handleDivClick = (id) => {
+
+    let clickArr = this.state.clicks
+    let { highScore, clickCount } = this.state
+
+    console.log(clickArr)
+
+    if (clickArr.indexOf(id) > -1) {
+     
+      this.setState({ clicks: [], clickCount: 0, losses: this.state.losses + 1,results: "loss" })
+
+
+
+    } else {
+
+      clickCount++
+
+      clickArr.push(id)
+      this.setState({ clicks: clickArr, clickCount: clickCount })
+
+
+
+      if (clickCount > highScore) {
+        this.setState({ highScore: clickCount })
+      }
+
+      if(clickCount % Helmets.length === 0){
+        this.setState({clicks:[], wins: this.state.wins + 1, results: "win" })
+      }else{
+        this.setState({results: "rules"})
+      }
+
+    }
+
+    this.setState({ Helmets: shuffleArray(Helmets) })
+
+  };
+  render() {
+   
+    return (
+
+      <div>
+
+        <Header clicks={this.state.clickCount} highScore={this.state.highScore} wins={this.state.wins} losses={this.state.losses} />
+
+        <GameBoard Helmets={this.state.Helmets} handleDivClick={this.handleDivClick} />
+
+        <ResultPanel display={this.state.results} />
+        
+      </div>
+
+
+    );
+  }
+
+};
+
+export default App;
