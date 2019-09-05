c60e3e78466f34e2d869385b6580a41c666352a7
System Administrator <root@Stevens-MacBook-Air.local>
2019-09-05 19:45:08 -0400

    Updates

---
+++ precache-manifest.98960d06e6180909f13fd8c3c2bdcdbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c40b4bb63bedf137fe2b78f9579fb9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3006489f0b4b839ec421",
+    "url": "/clicky-game/static/css/main.8d47dec6.chunk.css"
+  },
+  {
+    "revision": "36ff4c138fb4ac3c0a4b",
+    "url": "/clicky-game/static/js/2.333e8cac.chunk.js"
+  },
+  {
+    "revision": "3006489f0b4b839ec421",
+    "url": "/clicky-game/static/js/main.d52feaf4.chunk.js"
+  },
+  {
+    "revision": "6b626b11140007117b3b",
+    "url": "/clicky-game/static/js/runtime~main.6b9ff68f.js"
+  }
+]);
